802d043e47ab06c61579b3ca70ae6d634efca3a5
Juan Vidal <juanmvg2003@gmail.com>
2024-02-27 21:47:10 -0500

    creada funcionalidad de actualizar

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # importar Flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 #inicializar le proyecto
 app = Flask(__name__)
 
@@ -30,38 +30,61 @@ def add_persona():
     else:
         return render_template("add_person.html", mensaje="Error al agregar la persona!")
 
-
 # mostar las personas registradas
 @app.route('/personas-registradas')
 def render_personas():
     return render_template("show_person.html", personas=lista_personas)
 
-
-def persona_id():
-    for persona in lista_personas:
-        print(persona)
+# renderizando el form de actalizar persona
+@app.route('/editar-persona/<id>')
+def edit_person_render_form(id):
+    # almacenar persona
+    persona = None
+    # ncontrando persona
+    for person in lista_personas:
+        if person["id"] == id:
+            persona = person
+            break
+    return render_template("update_person.html", mensaje="Persona encontrada", persona = persona)
 
 # editando persona por id
-@app.route('/editar-persona/<id>')
-def edit_person(id):
-    # obteniendo el id de la persona a actualizar
-    cedula = id
-    print(cedula)
-    persona_id()
+@app.route('/editar-persona/<id>', methods=["POST"])
+def edit_person_by_id(id):
+    # almacenar persona
+    persona = None
+    # ncontrando persona
+    for person in lista_personas:
+        if person["id"] == id:
+            persona = person
+            break
         
     # verificando si hay una actualizacion de datos
-    if request.method == 'PUT':
+    if request.method == 'POST':
         # obteniendo datos del formulario
         cedula = request.form["cedula"]
         nombre = request.form["nombre"]
         mail = request.form["mail"]
         telefono = request.form["telefono"]
         
+        # persona actualizada
+        new_person = {"id": cedula, "nombre": nombre, "mail": mail, "telefono": telefono}
         
-        return render_template("update_person.html", mensaje="Persona encontrada")
+        # encontrar persona y obtener su índice
+        person_index = None
+        for index, person in enumerate(lista_personas):
+            if person["id"] == id:
+                person_index = index
+                break
+            
+        # actualizar la persona en su posicion actual
+        if person_index is not None:
+            lista_personas[person_index] = new_person
+        
+        return redirect("/personas-registradas")
     # en caso de no encontrar la persona
     else:
-        return render_template("update_person.html", mensaje="No fue encontrada la persona!")
+        return render_template("update_person.html", mensaje="No fue encontrada la persona!", persona = persona)
+
 
 # corriendo server
 if __name__ == '__main__':
